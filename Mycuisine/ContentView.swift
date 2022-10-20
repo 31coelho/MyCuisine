b8713b7896324aa66458708cf213317cd5b9f509
Tiago Coelho <tiago.coelho@paddypowerbetfair.com>
2022-10-20 20:51:11 +0100

    asdasd

---
+++ Mycuisine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mycuisine
+//
+//  Created by Tiago Coelho on 20/10/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mycuisine/MycuisineApp.swift
@@ -0,0 +1,17 @@
+//
+//  MycuisineApp.swift
+//  Mycuisine
+//
+//  Created by Tiago Coelho on 20/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct MycuisineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
