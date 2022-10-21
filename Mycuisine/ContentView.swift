9e900b1f594bfe11f69de83b272247b5650b6ebf
Tiago Coelho <tiago.coelho@paddypowerbetfair.com>
2022-10-21 01:13:46 +0100

    simple mockup and started search bar

--- Mycuisine/ContentView.swift
+++ Mycuisine/ContentView.swift
@@ -8,9 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var username: String = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            VStack {
+                
+                HStack {
+                    Image(systemName: "location.circle.fill").foregroundColor(.gray).padding(10)
+                    TextField("Search by name or location", text: $username)
+                        
+                }.overlay(
+                    RoundedRectangle(cornerRadius: 16)
+                        .stroke(Color(hex: "#989898"), lineWidth: 0.8)
+                )
+               
+                HStack {
+                    Rectangle().foregroundColor(.pink)
+                    Rectangle().foregroundColor(.blue)
+                    Rectangle().foregroundColor(.red)
+                }
+                HStack {
+                    Rectangle().foregroundColor(.pink)
+                    Rectangle().foregroundColor(.blue)
+                    Rectangle().foregroundColor(.red)
+                }
+            }.padding()
+            Rectangle().foregroundColor(.green)
+        }
+        
     }
 }
 
